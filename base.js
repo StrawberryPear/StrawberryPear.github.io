6ba5374187c8ba48816c1f4cd6068c891c5e38ac
Pear <perryfraser@gmail.com>
2023-03-14 15:22:38 +1100

    updates

--- base.js
+++ base.js
@@ -51,7 +51,7 @@ const awaitFrame = () => new Promise(resolve => {
 
 const updateDeck = () => {
   // work out total points in deck :D
-  const cards = deck.map(deckStore => [deckStore, ...(deckStore.upgrades || [])]).flat().map(deckStore => store[deckStore.uid]);
+  const cards = deck.map(deckStore => [deckStore, ...(deckStore.upgrades || [])]).flat().map(deckStore => cardsStore[deckStore.uid]);
   const cost = cards.reduce((sum, card) => card ? sum + (parseInt(card.cost) || 0) : sum, 0);
 
   const deckCostEle = document.getElementById('points');
@@ -215,7 +215,7 @@ const addCharacterToDeck = (data, updateDeckStore = true) => {
   const uid = data.uid;
   if (!uid) return;
 
-  const cardStore = store[uid];
+  const cardStore = cardsStore[uid];
   if (!cardStore) return;
 
   const libraryCardEles = [...cardLibraryListEle.children];
@@ -263,7 +263,7 @@ const addCharacterToDeck = (data, updateDeckStore = true) => {
 const addUpgradeToCharacter = (upgradeUID, characterCardEle, deckCharacter, updateDeckStore = true) => {
   if (!upgradeUID) return;
 
-  const upgradeCardStore = store[upgradeUID];
+  const upgradeCardStore = cardsStore[upgradeUID];
   if (!upgradeCardStore) return;
 
   const libraryCardEles = [...cardLibraryListEle.children];
@@ -351,7 +351,21 @@ const init = async () => {
     const doc = document.documentElement;
     doc.style.setProperty('--screen-height', `${window.innerHeight}px`);
     doc.style.setProperty('--screen-width', `${window.innerWidth}px`);
+
+    window?.plugins?.safearea?.get(
+      (result) => {
+        // elegantly set the result somewhere in app state
+        doc.style.setProperty('--safe-area-top', `${result.top}px`);
+        doc.style.setProperty('--safe-area-bottom', `${result.bottom}px`);
+
+        doc.style.setProperty('--screen-height', `${window.innerHeight - result.top - result.bottom}px`);
+      },
+      (error) => {
+        // maybe set some sensible fallbacks?
+      }
+    );
   }
+  document.addEventListener("resume", updateAppSize);
   window.addEventListener('resize', updateAppSize)
   updateAppSize()
 
@@ -499,7 +513,7 @@ const init = async () => {
     }
 
     const uid = deckCurrentCardEle.getAttribute("uid");
-    const cardStore = store[uid];
+    const cardStore = cardsStore[uid];
     if (!cardStore) return;
 
     const currentCardIndex = [...cardDeckListEle.children].indexOf(deckCurrentCardEle.parentElement);
@@ -588,7 +602,7 @@ const init = async () => {
     showToast(`Upgrade Attached`);
     showDeckButton.click();
   });
-  cardScrollerEle.addEventListener('click', (e) => {
+  cardScrollerEle.addEventListener('click', async (e) => {
     // get the card that was clicked
     const cardEle = e.target;
 
@@ -616,7 +630,7 @@ const init = async () => {
     if (!currentCardEle) return;
     
     const uid = currentCardEle.getAttribute("uid");
-    const cardStore = store[uid];
+    const cardStore = cardsStore[uid];
     if (!cardStore) return;
 
     // get the xy of where was clicked
@@ -633,8 +647,15 @@ const init = async () => {
 
     switch (currentCardEle.tagName) {
       case "PURCHASE":
-        if (!currentCardEle.classList.contains("bought")) {
-          window.open("https://relicblade.com/shop?category=Cards");
+        try {
+          const status = await buyProduct(uid);
+
+          if (status) {
+            // install the cards.
+            await installProduct(uid);
+          }
+        } catch (error) {
+          showToast(error.message);
         }
         break;
     }
@@ -809,6 +830,8 @@ const init = async () => {
   })
 
   const onCarouselInteraction = event => {
+    if (!event.touches) return;
+
     const touch = event.touches[0];
     const touchX = touch.clientX - carouselCanvasEle.offsetLeft;
     const scrollRatio = touchX / carouselCanvasEle.clientWidth;
